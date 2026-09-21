04d6ea35f66cf6f00c7017ae7d6e5e1ca4e75fdf
Vivek Bisht <vivekvb2124@gmail.com>
2026-09-21 14:05:48 +0530

    Fix closing tag for html element in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+    </html
   );
 }
